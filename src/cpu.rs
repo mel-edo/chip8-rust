59d93840b36ee2487fe2144087c9b1f6198cb105
mel-edo <meledo@duck.com>
2025-07-17 00:24:38 +0530

    Added opcode fetching

--- src/cpu.rs
+++ src/cpu.rs
@@ -31,6 +31,39 @@ impl Cpu {
         }
         Ok(())
     }
+
+    pub fn cycle(&mut self) -> Result<(), std::io::Error> {
+        // Fetch the opcode
+
+        // suppose memory[pc] = 0xA2 -> binary: 10100010 (this is u8)
+        // memory[pc+1] = 0xF0 -> binary: 11110000 (also u8)
+        
+        // 0xA2 gets casted as u16 -> 00000000 10100010
+        // Thus, we will shift it by 8
+
+        // Now we have 10100010 00000000
+        // and the second 00000000 11110000
+        // we can simply xor them to get the u16 opcode
+
+        self.opcode = (self.memory[self.pc] as u16) << 8 | (self.memory[self.pc + 1]) as u16;
+
+        println!("Opcode: {:#06x}, pc: {}", self.opcode, self.pc);
+
+        // increment program counter by 2
+        self.pc += 2;
+
+        // decode the opcode
+        // execute the instruction
+        
+        // update timers if needed(decrement both timers if they are > 0)
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        } else if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        };
+
+        Ok(())
+    }
 }
 
 pub struct Cpu {
@@ -38,7 +71,7 @@ pub struct Cpu {
     memory: [u8; 4096],
     v: [u8; 16],
     i: u16,
-    pc: u16,
+    pc: usize,
     gfx: [u8; 64*32],
     sound_timer: u8,
     delay_timer: u8,
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,9 @@ use cpu::Cpu;
 fn main() {
     let mut cpu = Cpu::new();
 
-    Cpu::load_rom(&mut cpu, "../c8games/PONG");
+    Cpu::load_rom(&mut cpu, "c8games/PONG");
+
+    for _ in 0..4 {
+        Cpu::cycle(&mut cpu);
+    };
 }
